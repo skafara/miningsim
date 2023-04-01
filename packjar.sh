01aeb6d58fe3e21b0dff45498ade9cc184d07229
Stanislav Kafara <skafara@students.zcu.cz>
2023-04-01 13:21:01 +0200

    Update pack jar script not to include not necessary documentation files

--- packjar.sh
+++ packjar.sh
@@ -1,7 +1,7 @@
 bash build.sh
 bash makedoc.sh
 jar cvf app.jar -C bin miningsim
-jar uvf app.jar doc src
+jar uvf app.jar doc/dokumentace.pdf doc/javadoc src
 mkdir tmp-packjar
 mkdir -p tmp-packjar/META-INF
 echo "Main-Class: miningsim.Main" > ./tmp-packjar/META-INF/MANIFEST.MF
