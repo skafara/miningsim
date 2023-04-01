a54cd1e7b9fd2a5b416866e61c2f73d4a2ba4fab
Stanislav Kafara <skafara@students.zcu.cz>
2023-04-01 13:17:36 +0200

    Update jar pack script not to duplicate Manifest-Version attribute

--- packjar.sh
+++ packjar.sh
@@ -4,7 +4,6 @@ jar cvf app.jar -C bin miningsim
 jar uvf app.jar doc src
 mkdir tmp-packjar
 mkdir -p tmp-packjar/META-INF
-echo "Manifest-Version: 1.0" > ./tmp-packjar/META-INF/MANIFEST.MF
-echo "Main-Class: miningsim.Main" >> ./tmp-packjar/META-INF/MANIFEST.MF
+echo "Main-Class: miningsim.Main" > ./tmp-packjar/META-INF/MANIFEST.MF
 jar uvmf tmp-packjar/META-INF/MANIFEST.MF app.jar
 rm -rf tmp-packjar
